ec9883bd5f246e205b4a9bcd0e06d508ff8b6437
gavin <gavinaa123aah@163.com>
2018-11-23 23:35:08 +0800

    第八章完成/实现博客新增，修改，查看功能

--- blog/views.py
+++ blog/views.py
@@ -14,6 +14,29 @@ def index(request):
     return render(request, 'blog/index.html', {'articles': articles})
 
 
-def artice_page(request, article_id):
+def article_page(request, article_id):
     article = models.Artical.objects.get(pk=article_id)
     return render(request, 'blog/article_page.html', {'article': article})
+
+
+def edit_page(request, article_id):
+    if str(article_id) == '0':
+        return render(request, 'blog/edit_page.html')
+    article = models.Artical.objects.get(pk=article_id)
+    return render(request, 'blog/edit_page.html', {'article': article})
+
+
+def edit_action(request):
+    title = request.POST.get('title', 'default value')
+    content = request.POST.get('content', 'default value')
+    article_id = request.POST.get('article_id', '0')
+    if article_id == '0':
+        models.Artical.objects.create(title=title, content=content)
+        articles = models.Artical.objects.all()
+        return render(request, 'blog/index.html', {'articles': articles})
+    else:
+        article = models.Artical.objects.get(pk=article_id)
+        article.title = title
+        article.content = content
+        article.save()
+        return render(request, 'blog/article_page.html', {'article': article})

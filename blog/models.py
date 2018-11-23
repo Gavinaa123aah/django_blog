cf1ed6f05b5a34c3abe88ae13ed32c8cbbbf065a
gavin <gavinaa123aah@163.com>
2018-11-23 21:12:44 +0800

    第六章完成

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class Artical(models.Model):
+    title = models.CharField(max_length=32, default='title')
+    content = models.TextField(null=True)
--- blog/views.py
+++ blog/views.py
@@ -4,8 +4,11 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 
 # Create your views here.
+
 from django.http import HttpResponse
+from . import models
 
 
 def index(request):
-    return render(request, 'blog/index.html', {'hello':'hello blog!!!'})
+    article = models.Artical.objects.get(pk=1)
+    return render(request, 'blog/index.html', {'article': article})

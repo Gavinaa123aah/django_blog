6471a62b7d065cf2efafd1f040cb192c2e284ab1
gavin <gavinaa123aah@163.com>
2018-11-23 21:25:14 +0800

    第七章完成

--- blog/models.py
+++ blog/models.py
@@ -9,3 +9,6 @@ from django.db import models
 class Artical(models.Model):
     title = models.CharField(max_length=32, default='title')
     content = models.TextField(null=True)
+
+    def __unicode__(self):
+        return self.title
